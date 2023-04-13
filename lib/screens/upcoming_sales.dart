3c379326dbef86b69fd5d8ce039cf4e1682e70f6
Tonderai <tonderaimadende3@gmail.com>
2023-04-13 13:41:54 +0200

    added upcoming sales

--- lib/screens/upcoming_sales.dart
+++ lib/screens/upcoming_sales.dart
@@ -36,7 +36,116 @@ class UpcomingSales extends StatelessWidget {
                 children: [
                   SizedBox(
                     width: width,
-                      child: Image.asset("assets/images/couch.jpeg", height: height*0.3, width: width,))
+                      child: Image.asset("assets/images/couch.jpeg",height: height*0.3,  width: width, fit: BoxFit.fill,)
+                  ),
+                  Container(
+                    height: height*0.3,
+                      width: width,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.white, Colors.white.withOpacity(0.5), Colors.black54.withOpacity(0.4)]
+                      )
+
+                    ),
+                  ),
+                  SizedBox(
+                    height: height*0.3,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 10,),
+                              SizedBox(
+                                  width: width*0.3,
+                                  // height: height*0.3,
+                                  child: Column(
+                                    children: [
+                                      SizedBox(height: height*0.01,),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          SizedBox(width: 10,),
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text("MON"),
+                                              Text("3", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+                                              Text("APR")
+                                            ],
+                                          ),
+                                          Text(">", style: TextStyle(fontSize: 30,),),
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text("MON"),
+                                              Text("3", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+                                              Text("APR")
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+
+                                    ],
+                                  )
+                              )
+
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: width*0.1,),
+                              Icon(Icons.home_outlined, size: width*.25, color: Colors.orangeAccent,),
+                              SizedBox(width: width*0.01,),
+                              Row(
+                                children: [
+                                  Container(
+                                    width: width*0.04,
+                                    height: height*0.06,
+                                    decoration: BoxDecoration(
+                                      color: Colors.orangeAccent
+                                    ),
+                                  ),
+                                  Column(
+                                    children: [
+                                      Container(
+                                        height: height*0.03,
+                                        width: width*0.6,
+                                        decoration: BoxDecoration(
+                                            color: Colors.indigo[900]
+                                        ),
+                                        child: Row(
+                                          children: [
+                                            SizedBox(width: width*.02,),
+                                            Icon(Icons.home_outlined, color: Colors.white, size: 20,),
+                                            SizedBox(width: width*.02,),
+                                            Text("HRE Goods", style: TextStyle(color: Colors.white),)
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        height: height*0.03,
+                                        width: width*0.6,
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey[800]
+                                        ),
+                                        child: Row(
+                                          children: [
+                                            SizedBox(width: width*.02,),
+                                            Icon(Icons.flag, color: Colors.white, size: 20,),
+                                            SizedBox(width: width*.02,),
+                                            Text("Harare, Zimbabwe", style: TextStyle(color: Colors.white),)
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                  )
                 ],
 
               ),
